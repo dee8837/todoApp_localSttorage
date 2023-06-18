4f69d87e8355ccadcec1206e68448ad1a4c4b9da
Deepak Shahi <96306408+dee8837@users.noreply.github.com>
2023-06-18 18:15:51 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+
+import { Form } from "./components/Form";
+
+function App() {
+
+  return (
+    <div className="wrapper">
+      <h3>TODOAPP USING REACT HOOKS AND LOCALSTORAGE</h3>
+      <div className="form-and-todo-box">
+        <Form/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
+
+
